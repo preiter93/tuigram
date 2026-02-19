f34b9feffed4e7b37e6f5cd718a5378b2ecd8908
Philipp Reiter <p.reiter93@gmail.com>
2026-02-19 19:45:37 +0100

    chore(release): v0.1.3

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         packages = {
           tuigram = pkgs.rustPlatform.buildRustPackage {
             pname = "tuigram";
-            version = "0.1.2";
+            version = "0.1.3";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
 
